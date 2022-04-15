80eac138caf69eda2b7bc8c82eee875c4d395c4f
adrianna-thomas <42939699+adrianna-thomas@users.noreply.github.com>
2022-04-15 18:56:44 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const ratingButtons = document.querySelectorAll(".rating-btn");
+const submitRatingBtn = document.getElementById("submitRatingBtn");
+const ratingCard = document.getElementById("ratingCard");
+const feedbackCard = document.getElementById("feedbackCard");
+const numberRating = document.getElementById("numberRating");
+let selectedRating = "";
+
+
+
+function clickRatingBtn({ target }) {
+    removeAllActiveClass();
+    target.classList.add("active");
+    selectedRating = target.value;
+}
+
+
+function removeAllActiveClass() {
+    ratingButtons.forEach((button) => button.classList.remove("active"));
+}
+
+
+function ratingSubmit() {
+    if (!selectedRating) {
+        return alert("Please select number rating!");
+    }
+
+    numberRating.textContent = selectedRating;
+    feedbackCard.classList.remove("hidden");
+    ratingCard.classList.add("hidden");
+}
+
+ratingButtons.forEach((button) => {
+    button.addEventListener("click", clickRatingBtn);
+});
+submitRatingBtn.addEventListener("click", ratingSubmit);
